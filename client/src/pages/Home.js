dd093adbde54154f6ab4e4f4a792ddc79bc30ddb
Gospel Chukwu <brondchux@gmail.com>
2021-09-26 10:56:53 -0400

    Broke-out salmon wallpaper into a separate component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,37 +1,14 @@
-import mealImage from "../assets/images/salmon-bg.jpg";
-import SearchBar from "../components/SearchBar";
-import { Grid, Header, Segment } from "semantic-ui-react";
+import { Header, Segment } from "semantic-ui-react";
+import Banner from "../components/Banner";
 import Carousel from "../components/Carousel";
 
 const Home = () => {
-	const customStyle = {
-		wallpaper: {
-			width: "100%",
-			minHeight: "300px",
-			backgroundImage: `url(${mealImage})`,
-			backgroundPosition: "center",
-			backgroundRepeat: "no-repeat",
-			backgroundSize: "cover",
-			display: "flex",
-			justifyContent: "center",
-			alignItems: "center",
-		},
-		search: {
-			minWidth: "60%",
-		},
-	};
 	return (
 		<>
-			<Grid columns={1}>
-				<div style={customStyle.wallpaper}>
-					<Grid.Row style={customStyle.search}>
-						<Grid.Column>
-							<SearchBar></SearchBar>
-						</Grid.Column>
-					</Grid.Row>
-				</div>
-			</Grid>
-			<Segment padded="very">
+			<Segment basic>
+				<Banner></Banner>
+			</Segment>
+			<Segment basic padded="very">
 				<Header as="h2">
 					<span className="cadet-color">Recently added dishes</span>
 				</Header>
