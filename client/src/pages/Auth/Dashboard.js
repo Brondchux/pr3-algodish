b392e86718d87c3e184448de7e87219b2e0f041f
Gospel Chukwu <brondchux@gmail.com>
2021-09-26 18:09:10 -0400

    Designed the dashboard display components

--- client/src/pages/Auth/Dashboard.js
+++ client/src/pages/Auth/Dashboard.js
@@ -1,5 +1,15 @@
-import { Segment, Header, Grid, Button, Icon } from "semantic-ui-react";
+import {
+	Segment,
+	Header,
+	Grid,
+	Button,
+	Icon,
+	Divider,
+} from "semantic-ui-react";
 import { buttonStyles } from "../../components/MainButton";
+import Carousel from "../../components/Carousel";
+// TODO: Remove me after fetching real dishes from db
+import { testDishList } from "../Home";
 
 const Dashboard = () => {
 	return (
@@ -8,16 +18,31 @@ const Dashboard = () => {
 				<Grid columns={2} stackable>
 					<Grid.Row>
 						<Grid.Column>
-							<Header as="h2">Welcome back, Elle!</Header>
+							<Header as="h2">
+								<Icon name="user outline"></Icon>
+								Welcome back, Elle!
+							</Header>
 						</Grid.Column>
 						<Grid.Column>
 							<Button fluid style={buttonStyles.algoButton} size="massive">
 								<Icon name="plus"></Icon>
-								Create a dish
+								Create new dish
 							</Button>
 						</Grid.Column>
 					</Grid.Row>
 				</Grid>
+
+				<Divider horizontal></Divider>
+				<Header as="h2">
+					<span className="cadet-color">Dishes you created</span>
+				</Header>
+				<Carousel dishList={testDishList()}></Carousel>
+
+				<Divider horizontal></Divider>
+				<Header as="h2">
+					<span className="cadet-color">Your favourite dishes</span>
+				</Header>
+				<Carousel dishList={testDishList()}></Carousel>
 			</Segment>
 		</Segment>
 	);
