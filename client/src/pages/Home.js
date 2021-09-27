35efcd2cb883d978f2934502580e28b677d36f2e
Gospel Chukwu <brondchux@gmail.com>
2021-09-27 16:41:50 -0400

    Successfully displayed the lastest four dishes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,78 +1,41 @@
 import { Header, Icon, Segment } from "semantic-ui-react";
 import Banner from "../components/Banner";
 import Carousel from "../components/Carousel";
-import spagettiImg from "../assets/images/spagetti.jpg";
-import { FETCH_ALL_DISHES } from "../utils/queries";
+import TestDishes from "../components/TestDishes";
+import { FETCH_LAST_FOUR_DISHES } from "../utils/queries";
 import { useQuery } from "@apollo/client";
 
-const generateRandomId = () => Math.floor(Math.random() * 99);
-export let testDishList = () => [
-  {
-    id: generateRandomId(),
-    image: spagettiImg,
-    title: `Delicious spagetti ${generateRandomId()}`,
-    username: "Elle",
-    cookTime: generateRandomId(),
-    description: "Spagetti cooked in the Zimbabwe style!",
-  },
-  {
-    id: generateRandomId(),
-    image: spagettiImg,
-    title: `Delicious spagetti ${generateRandomId()}`,
-    username: "Elle",
-    cookTime: generateRandomId(),
-    description: "Spagetti cooked in the Zimbabwe style!",
-  },
-  {
-    id: generateRandomId(),
-    image: spagettiImg,
-    title: `Delicious spagetti ${generateRandomId()}`,
-    username: "Elle",
-    cookTime: generateRandomId(),
-    description: "Spagetti cooked in the Zimbabwe style!",
-  },
-  {
-    id: generateRandomId(),
-    image: spagettiImg,
-    title: `Delicious spagetti ${generateRandomId()}`,
-    username: "Elle",
-    cookTime: generateRandomId(),
-    description: "Spagetti cooked in the Zimbabwe style!",
-  },
-];
-
 const Home = () => {
-  const { loading, data } = useQuery(FETCH_ALL_DISHES);
-  console.log(data);
-  testDishList = () => data?.allDishes || testDishList;
-  return (
-    <>
-      <Segment basic>
-        <Banner></Banner>
-      </Segment>
-      <Segment basic padded="very">
-        {loading ? (
-          <Segment placeholder raised textAlign="center">
-            <Header textAlign="center" size="large">
-              <Icon name="spinner"></Icon>
-            </Header>
-          </Segment>
-        ) : (
-          <>
-            <Header as="h2">
-              <span className="cadet-color">Recently added dishes</span>
-            </Header>
-            <Carousel dishList={testDishList()}></Carousel>
+	const { loading, data } = useQuery(FETCH_LAST_FOUR_DISHES);
+	const testDishList = () => data?.lastFourDishes || TestDishes;
+	return (
+		<>
+			<Segment basic>
+				<Banner></Banner>
+			</Segment>
+			<Segment basic padded="very">
+				{loading ? (
+					<Segment placeholder raised textAlign="center">
+						<Header textAlign="center" size="large">
+							<Icon name="spinner"></Icon>
+						</Header>
+					</Segment>
+				) : (
+					<>
+						<Header as="h2">
+							<span className="cadet-color">Recently added dishes</span>
+						</Header>
+						<Carousel dishList={testDishList()}></Carousel>
 
-            <Header as="h2">
-              <span className="cadet-color">Most popular dishes</span>
-            </Header>
-            <Carousel dishList={testDishList()}></Carousel>
-          </>
-        )}
-      </Segment>
-    </>
-  );
+						<Header as="h2">
+							<span className="cadet-color">Most popular dishes</span>
+						</Header>
+						<Carousel dishList={testDishList()}></Carousel>
+					</>
+				)}
+			</Segment>
+		</>
+	);
 };
 
 export default Home;
