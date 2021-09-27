3e5a76c25c9f043d0a54381f2d57e2fbef0f2ffd
zanama <57731386+zanmama@users.noreply.github.com>
2021-09-27 10:19:18 -0400

    added nav link for AboutUs to navbar.js

--- client/src/components/Header/navbar.js
+++ client/src/components/Header/navbar.js
@@ -2,24 +2,27 @@ import "./header.css";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-	return (
-		<nav>
-			<ul>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/dashboard">Dashboard</Link>
-				</li>
-				<li>
-					<Link to="/login">Login</Link>
-				</li>
-				<li>
-					<Link to="/signup">Signup</Link>
-				</li>
-			</ul>
-		</nav>
-	);
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/aboutus">About Us</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/signup">Signup</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
