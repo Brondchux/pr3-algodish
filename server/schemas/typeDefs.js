725f638d9f76acba1acc7aad2c4c804b09f278d9
Rachel Gregg <rgregg2014@gmail.com>
2021-09-27 16:55:09 -0400

    login mutation done, working on add new dish

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -57,8 +57,16 @@ const typeDefs = gql`
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    
-    uploadDish(title: String!, username: String!, decription: String!, image: String, ingredients: String!, recipe: String!): Dish
+
+    uploadDish(
+      title: String!
+      username: String!
+      description: String!
+      image: String
+      ingredients: String!
+      recipe: String!
+      cook_time: Int
+    ): Dish
     addInstructions(dishId: ID!): Dish
     addStep(instructionId: ID!, time: Int, step: String!): Dish
   }
