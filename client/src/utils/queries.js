e78102cbfbc123de5ba9ebdbe76be9de409b4f0c
Rachel Gregg <rgregg2014@gmail.com>
2021-09-27 14:46:41 -0400

    resolving conflict

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -28,7 +28,6 @@ export const FETCH_ALL_DISHES = gql`
 
 //Why is this suddenly not working
 export const FETCH_DISH_BY_ID = gql`
-<<<<<<< HEAD
   query dishById($id: ID!) {
     dishById(id: $id) {
       _id
@@ -39,22 +38,10 @@ export const FETCH_DISH_BY_ID = gql`
       cookTime
     }
   }
-=======
-	query dishById($id: ID!) {
-		dishById(id: $id) {
-			_id
-			title
-			dishAuthor
-			image
-			description
-		}
-	}
->>>>>>> 7c49279893066fe364129da00b035432339621ae
 `;
 
 //Get One Master Dish
 export const FETCH_WHOLE_DISH_BY_ID = gql`
-<<<<<<< HEAD
   query dishById($id: ID!) {
     dishById(id: $id) {
       title
@@ -66,23 +53,10 @@ export const FETCH_WHOLE_DISH_BY_ID = gql`
       recipe
     }
   }
-=======
-	query dishById($id: ID!) {
-		dishById(id: $id) {
-			title
-			dishAuthor
-			image
-			description
-			ingredients
-			recipe
-		}
-	}
->>>>>>> 7c49279893066fe364129da00b035432339621ae
 `;
 
 //Get all dishes by user ID (created_dishes)
 export const FETCH_CREATED_DISHES = gql`
-<<<<<<< HEAD
   query userDishes($id: ID!) {
     userDishes(id: $id) {
       title
@@ -93,22 +67,10 @@ export const FETCH_CREATED_DISHES = gql`
       ingredients
     }
   }
-=======
-	query userDishes($id: ID!) {
-		userDishes(id: $id) {
-			title
-			dishAuthor
-			ingredients
-			image
-			description
-		}
-	}
->>>>>>> 7c49279893066fe364129da00b035432339621ae
 `;
 
 //five random dishes
 export const FETCH_FOUR_RANDOM_DISHES = gql`
-<<<<<<< HEAD
   query fourRandomDishes {
     fourRandomDishes {
       title
@@ -118,21 +80,10 @@ export const FETCH_FOUR_RANDOM_DISHES = gql`
       cookTime
     }
   }
-=======
-	query fourRandomDishes {
-		fourRandomDishes {
-			title
-			dishAuthor
-			image
-			description
-		}
-	}
->>>>>>> 7c49279893066fe364129da00b035432339621ae
 `;
 
 //last five dishes
 export const FETCH_LAST_FOUR_DISHES = gql`
-<<<<<<< HEAD
   query lastFiveDishes {
     lastFiveDishes {
       title
@@ -142,21 +93,10 @@ export const FETCH_LAST_FOUR_DISHES = gql`
       cookTime
     }
   }
-=======
-	query lastFiveDishes {
-		lastFiveDishes {
-			title
-			dishAuthor
-			image
-			description
-		}
-	}
->>>>>>> 7c49279893066fe364129da00b035432339621ae
 `;
 
 //Get dishes by name
 export const FETCH_DISH_BY_NAME = gql`
-<<<<<<< HEAD
   query dishesByName($title: String!) {
     dishesByName(title: $title) {
       title
@@ -166,24 +106,14 @@ export const FETCH_DISH_BY_NAME = gql`
       cookTime
     }
   }
-=======
-	query dishesByName($title: String!) {
-		dishesByName(title: $title) {
-			title
-			dishAuthor
-			image
-			description
-		}
-	}
->>>>>>> 7c49279893066fe364129da00b035432339621ae
 `;
 
 //Get all users
 export const FETCH_USERS = gql`
-	query users {
-		users {
-			_id
-			username
-		}
-	}
+  query users {
+    users {
+      _id
+      username
+    }
+  }
 `;
