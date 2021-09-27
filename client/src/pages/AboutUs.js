7240d266b50405f972f4fcd31350c4690f671fbb
zanama <57731386+zanmama@users.noreply.github.com>
2021-09-27 07:33:57 -0400

    Creating the wording for the About Us page. Slogan created

---
+++ client/src/pages/AboutUs.js
@@ -0,0 +1,20 @@
+import { Header, Segment } from "semantic-ui-react";
+import Banner from "../components/Banner";
+import Carousel from "../components/Carousel";
+
+const AboutUs = () => {
+	return (
+        <p>It is a pleasure to have you here to explore recipes from around the globe and hopefully add dishes of your own that you would like to see others properly cook based on your specific timing and blending patterns. 
+            
+        This application was created by four developers whose backgrounds span the globe. In an effort to help their friends recreate traditional cuisines from their own homes without compromising on taste, we came together and built a recipe helper that can time the user and creator down to the second to aid them along in all steps of the recreation process. 
+        
+        As such, we titled this <strong> The algoDish</strong>, which in future development will run entirely on Machine Learning algorithms to help even the least cooking inclined person create a tasty dish.
+
+        We're excited to have you here, and would love to hear feedback on your experience as you explore the site and share dishes that speak to your soul. As long as you have access to a stove and ingredients, the algoDish can suit you. We pride ourselves on creating a seamless transition between the inception of the cooking project down to the finale of enjoyable eating the creation. 
+
+        Algorithms can help us improve in a number of areas. <strong>Proven, tried, and true, AlgoDish is here for you.</strong>
+        </p>
+
+
+
+export default AboutUs;
