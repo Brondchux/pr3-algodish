e4b28f486e1382c1b52ab1bbc19bca3917b52e33
Nafis Rahman <nmr278@nyu.edu>
2021-09-27 11:55:56 -0400

    added uploadDish to resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -63,24 +63,44 @@ const resolvers = {
             const user = await User.create({ username, email, password });
             const token = signToken(user);
             return { token, user };
-          },
-        login: async (_, { email, password }) => {
-        const user = await User.findOne({ email });
-    
-        if (!user) {
-            throw new AuthenticationError('No user found with this email address');
-        }
-    
-        const correctPw = await user.isCorrectPassword(password);
-    
-        if (!correctPw) {
-            throw new AuthenticationError('Incorrect credentials');
-        }
-    
-        const token = signToken(user);
-    
-        return { token, user };
         },
+        login: async (_, { email, password }) => {
+            const user = await User.findOne({ email });
+        
+            if (!user) {
+                throw new AuthenticationError('No user found with this email address');
+            }
+        
+            const correctPw = await user.isCorrectPassword(password);
+        
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+        
+            const token = signToken(user);
+        
+            return { token, user };
+        },
+
+        // uploadDish(
+            // title: String!, 
+            // dishAuthor: String!, 
+            // decription: String!, 
+            // image: String, 
+            // ingredients: [String]!, 
+            // instructions: [String]!)
+
+        uploadDish: async (_, { title, dishAuthor, description, image, ingredients, recipe }, context) => {
+            if (context.user) {
+                const newDish = new Dish({ title, dishAuthor, description, image, ingredients, recipe });
+
+                await User.findByIdAndUpdate(context.user.id, {
+                    $push: {created_dishes: newDish}
+                })
+
+                return newDish; 
+            };
+        }
     }
 
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -16,8 +16,9 @@ const typeDefs = gql`
     dishAuthor: String
     decription: String
     image: String
-    ingredients: [String]
-    instructions: Instructions!
+    ingredients: String
+    recipe: String
+    instructions: Instructions
   }
 
   type Instructions {
@@ -33,7 +34,7 @@ const typeDefs = gql`
   }
 
   type Auth {
-    token: ID!
+    token: ID
     user: User
   }
 
@@ -54,7 +55,8 @@ const typeDefs = gql`
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    uploadDish(title: String!, image: String!, ingredients: [String]!): Dish
+    
+    uploadDish(title: String!, dishAuthor: String!, decription: String!, image: String, ingredients: String!, recipe: String!): Dish
     addInstructions(dishId: ID!): Dish
     addStep(instructionId: ID!, time:Int, step: String!): Dish
     
