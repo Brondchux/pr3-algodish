679990ae1474d3d90182d6bc7403776e719f42fa
Rachel Gregg <rgregg2014@gmail.com>
2021-09-27 10:17:30 -0400

    finished query allDishes

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,22 +2,25 @@ import { gql } from "@apollo/client";
 
 //Get all dishes from the database
 export const FETCH_ALL_DISHES = gql`
-{
-    dishes {
-        _id
-        title
-        dishAuthor
-        image
-        instruction [{
-            _id
-            total_time
-            steps [{
-                step
-            }]
-        }]
+  {
+    allDishes {
+      _id
+      title
+      dishAuthor
+      image
     }
-}
+  }
+`;
+
+//Need a query dish by _id
+
+export const FETCH_DISH_BY_ID = gql`
+  {
+    dishById
+  }
 `;
 
 //Get all dishes by user ID (created_dishes)
 export const FETCH_CREATED_DISHES = gql``;
+
+// 6151cdb5d8d6a34560880263
