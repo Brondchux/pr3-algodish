4d4e6e55c430cf1eb2a06a6f96cd4b4169255323
zanama <57731386+zanmama@users.noreply.github.com>
2021-09-27 07:43:20 -0400

    Updated the wording of the AboutUs page

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -3,18 +3,36 @@ import Banner from "../components/Banner";
 import Carousel from "../components/Carousel";
 
 const AboutUs = () => {
-	return (
-        <p>It is a pleasure to have you here to explore recipes from around the globe and hopefully add dishes of your own that you would like to see others properly cook based on your specific timing and blending patterns. 
-            
-        This application was created by four developers whose backgrounds span the globe. In an effort to help their friends recreate traditional cuisines from their own homes without compromising on taste, we came together and built a recipe helper that can time the user and creator down to the second to aid them along in all steps of the recreation process. 
-        
-        As such, we titled this <strong> The algoDish</strong>, which in future development will run entirely on Machine Learning algorithms to help even the least cooking inclined person create a tasty dish.
-
-        We're excited to have you here, and would love to hear feedback on your experience as you explore the site and share dishes that speak to your soul. As long as you have access to a stove and ingredients, the algoDish can suit you. We pride ourselves on creating a seamless transition between the inception of the cooking project down to the finale of enjoyable eating the creation. 
-
-        Algorithms can help us improve in a number of areas. <strong>Proven, tried, and true, AlgoDish is here for you.</strong>
-        </p>
-
-
+  return (
+    <p>
+      It is a pleasure to have you here to explore recipes from around the
+      globe; we welcome your uniqueness and hope you will add dishes of your own
+      that you would like to see others prepare to perfection based on your
+      specific mixing, heating and blending patterns. This application was
+      created by four full-stack developers who attended Columbia Engineering
+      together in NYC. Our backgrounds span the globe and each foodie in our
+      group was perplexed by one specific cooking issue. We have specific dishes
+      that we wanted others to make, yet these other people would fall short
+      consistently. We knew we had to do something to save the integrety of our
+      personalized dishes, and we wanted to see people succeed in recreating our
+      masterpieces! In an effort to help our friends recreate our traditional
+      cuisines without compromising on taste and quality, we decided to come
+      together and build a recipe helper that can algorithmically time the user
+      and creator down to the second to aid them along in all steps of the
+      instructional and recreation process. As such, we titled this{" "}
+      <strong> The algoDish</strong>, which in future development will run
+      entirely on Machine Learning algorithms to help even the least cooking
+      inclined person create a tasty dish. We're excited to have you here, and
+      as we are a work in progress towards perfection, we would love to hear
+      feedback on your experience as you explore the site and share dishes that
+      speak to your soul. As long as you have access to a stove and ingredients,
+      the algoDish can suit you. We pride ourselves on creating a seamless
+      transition between the inception of the cooking project down to the finale
+      of enjoyably eating your creation. Algorithms can help us improve in a
+      number of areas, so why not with food?{" "}
+      <strong>Proven, tried, and true, AlgoDish is here for you.</strong>
+    </p>
+  );
+};
 
 export default AboutUs;
