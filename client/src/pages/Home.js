0230e2dc689fd4c0917aa0f67ea64af445b47d58
Rachel Gregg <rgregg2014@gmail.com>
2021-09-27 15:40:29 -0400

    troubleshooting react compile fail

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,72 +7,72 @@ import { useQuery } from "@apollo/client";
 
 const generateRandomId = () => Math.floor(Math.random() * 99);
 export let testDishList = () => [
-	{
-		id: generateRandomId(),
-		image: spagettiImg,
-		title: `Delicious spagetti ${generateRandomId()}`,
-		username: "Elle",
-		cookTime: generateRandomId(),
-		description: "Spagetti cooked in the Zimbabwe style!",
-	},
-	{
-		id: generateRandomId(),
-		image: spagettiImg,
-		title: `Delicious spagetti ${generateRandomId()}`,
-		username: "Elle",
-		cookTime: generateRandomId(),
-		description: "Spagetti cooked in the Zimbabwe style!",
-	},
-	{
-		id: generateRandomId(),
-		image: spagettiImg,
-		title: `Delicious spagetti ${generateRandomId()}`,
-		username: "Elle",
-		cookTime: generateRandomId(),
-		description: "Spagetti cooked in the Zimbabwe style!",
-	},
-	{
-		id: generateRandomId(),
-		image: spagettiImg,
-		title: `Delicious spagetti ${generateRandomId()}`,
-		username: "Elle",
-		cookTime: generateRandomId(),
-		description: "Spagetti cooked in the Zimbabwe style!",
-	},
+  {
+    id: generateRandomId(),
+    image: spagettiImg,
+    title: `Delicious spagetti ${generateRandomId()}`,
+    username: "Elle",
+    cookTime: generateRandomId(),
+    description: "Spagetti cooked in the Zimbabwe style!",
+  },
+  {
+    id: generateRandomId(),
+    image: spagettiImg,
+    title: `Delicious spagetti ${generateRandomId()}`,
+    username: "Elle",
+    cookTime: generateRandomId(),
+    description: "Spagetti cooked in the Zimbabwe style!",
+  },
+  {
+    id: generateRandomId(),
+    image: spagettiImg,
+    title: `Delicious spagetti ${generateRandomId()}`,
+    username: "Elle",
+    cookTime: generateRandomId(),
+    description: "Spagetti cooked in the Zimbabwe style!",
+  },
+  {
+    id: generateRandomId(),
+    image: spagettiImg,
+    title: `Delicious spagetti ${generateRandomId()}`,
+    username: "Elle",
+    cookTime: generateRandomId(),
+    description: "Spagetti cooked in the Zimbabwe style!",
+  },
 ];
 
 const Home = () => {
-	const { loading, data } = useQuery(FETCH_ALL_DISHES);
-	console.log(data);
-	testDishList = () => data?.allDishes || testDishList;
-	return (
-		<>
-			<Segment basic>
-				<Banner></Banner>
-			</Segment>
-			<Segment basic padded="very">
-				{loading ? (
-					<Segment placeholder raised textAlign="center">
-						<Header textAlign="center" size="large">
-							<Icon name="spinner"></Icon>
-						</Header>
-					</Segment>
-				) : (
-					<>
-						<Header as="h2">
-							<span className="cadet-color">Recently added dishes</span>
-						</Header>
-						<Carousel dishList={testDishList()}></Carousel>
+  const { loading, data } = useQuery(FETCH_ALL_DISHES);
+  console.log(data);
+  testDishList = () => data?.allDishes || testDishList;
+  return (
+    <>
+      <Segment basic>
+        <Banner></Banner>
+      </Segment>
+      <Segment basic padded="very">
+        {loading ? (
+          <Segment placeholder raised textAlign="center">
+            <Header textAlign="center" size="large">
+              <Icon name="spinner"></Icon>
+            </Header>
+          </Segment>
+        ) : (
+          <>
+            <Header as="h2">
+              <span className="cadet-color">Recently added dishes</span>
+            </Header>
+            <Carousel dishList={testDishList()}></Carousel>
 
-						<Header as="h2">
-							<span className="cadet-color">Most popular dishes</span>
-						</Header>
-						<Carousel dishList={testDishList()}></Carousel>
-					</>
-				)}
-			</Segment>
-		</>
-	);
+            <Header as="h2">
+              <span className="cadet-color">Most popular dishes</span>
+            </Header>
+            <Carousel dishList={testDishList()}></Carousel>
+          </>
+        )}
+      </Segment>
+    </>
+  );
 };
 
 export default Home;
