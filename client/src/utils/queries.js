7219dc2ba25760d420bb3bd46c343a4da76bcfff
Rachel Gregg <rgregg2014@gmail.com>
2021-09-27 10:46:36 -0400

    Added get dish by id

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -36,3 +36,9 @@ export const FETCH_CREATED_DISHES = gql``;
 
 // Dish _id for "Grilled Cheese"
 // 6151cdb5d8d6a34560880263
+
+//Get all users
+export const FETCH_USERS = gql``;
+
+//Get users by id
+export const FETCH_USER_BY_ID = gql``;
