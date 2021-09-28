98e405976823d16d92c98025d962066a80b69cc3
Gospel Chukwu <brondchux@gmail.com>
2021-09-28 01:42:29 -0400

    Dynamically rendered navbar based on authentication

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -38,9 +38,14 @@ const Login = () => {
 				variables: { ...formState },
 			});
 
-			Auth.login(data.login.token);
+			Auth.login(data.login);
 		} catch (e) {
 			console.error(e);
+		}
+
+		// redirect user to dashboard
+		if (Auth.loggedIn()) {
+			window.location = `/dashboard/${Auth.getUserId()}`;
 		}
 
 		// clear form values
@@ -56,7 +61,7 @@ const Login = () => {
 				<Grid.Row>
 					<Grid.Column></Grid.Column>
 					<Grid.Column>
-						{data ? (
+						{Auth.loggedIn() ? (
 							<Header as="h3" textAlign="center">
 								<p>Success! You are now logged in!</p>
 								<Divider horizontal></Divider>
