6e5ddc7c7cbf640e16e37e964064dc30b9cbd5ed
Nafis Rahman <nmr278@nyu.edu>
2021-10-06 12:48:03 -0400

    added ingredients list with highlight feature to cook page

--- client/src/pages/Cook.js
+++ client/src/pages/Cook.js
@@ -1,4 +1,4 @@
-import { Header, Icon, Segment, Message, Progress, Button } from "semantic-ui-react";
+import { Header, Icon, Segment, Message, Progress, Button, List } from "semantic-ui-react";
 import { FETCH_WHOLE_DISH_BY_ID } from "../utils/queries";
 import MainButton from "../components/MainButton";
 import { useQuery } from "@apollo/client";
@@ -24,13 +24,15 @@ const Cook = () => {
 		cook_time,
 	} = data?.dishById || {};
 
+    const ingredientsList = ingredients.split(',').map(item => item.trim())
+
     const [cookState, setCookState] = useState({
         totalCookTime: cook_time,
         totalTimePassed: 0,
         currenStepTimePassed: 0,
-        currentStep: 0,
         steps: instructions,
-        currentStep: 0
+        currentStep: 0,
+        cookTimer: ''
     })
     
     const incrementStep = (event) => {
@@ -61,7 +63,31 @@ const Cook = () => {
         };
     };
 
+    const minutesToTimeString = (seconds) => {
+        const minutesAsString = Math.floor(seconds/60);
+        const secondsAsString = seconds % 60;
+
+        return `${minutesAsString}:${secondsAsString}`;
+    }
+
+    const startTimer = (event) => {
+        event.preventDefault();
+        let currentStepTime = cookState.steps[cookState.currentStep]["time"]*60;
+        console.log(currentStepTime)
+        setInterval(() => { 
+            if(currentStepTime){
+                currentStepTime--;
+                setCookState({
+                    ...cookState,
+                    cookTimer: minutesToTimeString(currentStepTime)
+                })
+            };  
+        } , 1000);
+    }
+    
+
     console.log(cookState.steps)
+    console.log(ingredientsList)
 
     return (
         <>
@@ -70,6 +96,7 @@ const Cook = () => {
             <div>
                 <Progress percent={33} size="big"/>
                 <Progress percent={cookState.currentStep/(cookState.steps.length-1) * 100} size="big" indicating />
+                {cookState.cookTimer}
             </div>
             {loading ? (
                 <Loading></Loading>
@@ -82,11 +109,25 @@ const Cook = () => {
                     <Header textAlign="center">
                         <Button.Group>
                             <Button onClick={decrementStep} labelPosition='left' icon='left chevron' content='Back' />
+                            <Button onClick={startTimer} labelPosition='right' icon='play' />
                             <Button onClick={incrementStep} labelPosition='right' icon='right chevron' content='Forward' />
                         </Button.Group>
                     </Header>
                     <Message>
                         <Message.Header>Ingredients</Message.Header>
+                        <List link>
+                            {/* <List.Item active as='a' target="_blank" href='https://en.wikipedia.org/wiki/Thyme'>Home</List.Item>
+                            <List.Item as='a' target="_blank">About</List.Item>
+                            <List.Item as='a' target="_blank">Jobs</List.Item>
+                            <List.Item as='a' target="_blank">Team</List.Item> */}
+                            { ingredientsList.map( ingredient => {
+                                if (cookState.steps[cookState.currentStep].step.includes(ingredient)){
+                                    return <List.Item active as='a' target="_blank" href={`https://en.wikipedia.org/wiki/${ingredient}`}>{ingredient}</List.Item>
+                                } else {
+                                    return <List.Item as='a' target="_blank" href={`https://en.wikipedia.org/wiki/${ingredient}`}>{ingredient}</List.Item>
+                                }
+                            }) }
+                        </List>
                     </Message>                     
                 </Segment>
             )}
